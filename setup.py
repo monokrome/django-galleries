b6822d5bada3d6f9dc1f1cdeb68ae78e12c70251
Brandon R. Stoner <monokrome@monokro.me>
2010-08-01 22:50:07 -0600

    Added a setup file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='django-galleries',
+      version='0.2',
+      description='A small application that uses ImageKit to provide support for basic image galleries.',
+      author='Brandon R. Stoner',
+      author_email='monokrome@monokro.me',
+      url='http://monokro.me/',
+      packages=['django.contrib.galleries'],
+)
+
